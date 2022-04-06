12b1b0e003491f465b5cb14641a84b6c3d07c7ef
Lawali <ingu9981@naver.com>
2022-04-07 00:21:08 +0900

    파일 추가

--- VQA_Ingu/trainer.py
+++ VQA_Ingu/trainer.py
@@ -5,3 +5,70 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.optim import lr_scheduler
+
+device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def main(args):
+    os.makedirs(args.log_dir, exist_ok=True)
+    os.makedirs(args.model_dir, exist_ok=True)
+
+    
+
+if __name__=='main':
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--input_dir', type=str, default='./datasets',
+                        help='input directory for visual question answering.')
+
+    parser.add_argument('--log_dir', type=str, default='./logs',
+                        help='directory for logs.')
+
+    parser.add_argument('--model_dir', type=str, default='./models',
+                        help='directory for saved models.')
+
+    parser.add_argument('--max_qst_length', type=int, default=30,
+                        help='maximum length of question. \
+                              the length in the VQA dataset = 26.')
+
+    parser.add_argument('--max_num_ans', type=int, default=10,
+                        help='maximum number of answers.')
+
+    parser.add_argument('--embed_size', type=int, default=1024,
+                        help='embedding size of feature vector \
+                              for both image and question.')
+
+    parser.add_argument('--word_embed_size', type=int, default=300,
+                        help='embedding size of word \
+                              used for the input in the LSTM.')
+
+    parser.add_argument('--num_layers', type=int, default=2,
+                        help='number of layers of the RNN(LSTM).')
+
+    parser.add_argument('--hidden_size', type=int, default=512,
+                        help='hidden_size in the LSTM.')
+
+    parser.add_argument('--learning_rate', type=float, default=0.001,
+                        help='learning rate for training.')
+
+    parser.add_argument('--step_size', type=int, default=10,
+                        help='period of learning rate decay.')
+
+    parser.add_argument('--gamma', type=float, default=0.1,
+                        help='multiplicative factor of learning rate decay.')
+
+    parser.add_argument('--num_epochs', type=int, default=30,
+                        help='number of epochs.')
+
+    parser.add_argument('--batch_size', type=int, default=80,
+                        help='batch_size.')
+
+    parser.add_argument('--num_workers', type=int, default=0,
+                        help='number of processes working on cpu.')
+
+    parser.add_argument('--save_step', type=int, default=1,
+                        help='save step of model.')
+
+    args = parser.parse_args()
+
+    main(args)
