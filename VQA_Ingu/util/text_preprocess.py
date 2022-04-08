b8a3d55414823c376344c22bc0db190e053e17c5
Lawali <ingu9981@naver.com>
2022-04-09 00:31:05 +0900

    text_preprocess.py 추가 및 추상화

---
+++ VQA_Ingu/util/text_preprocess.py
@@ -0,0 +1,13 @@
+import os
+import argparse
+import numpy as np
+import json
+import re
+
+
+def make_vocab_questions(input_dir):
+    pass
+
+
+def make_vocab_answers(input_dir, n_answers):
+    pass
