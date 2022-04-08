456055381f0a09fc5d3be78ffc52013eb54a4214
Lawali <ingu9981@naver.com>
2022-04-09 00:32:33 +0900

    input_maker.py 추가 및 추상화

---
+++ VQA_Ingu/util/input_maker.py
@@ -0,0 +1,13 @@
+import numpy as np
+import json
+import os
+import argparse
+import text_helper
+from collections import defaultdict
+
+
+def extract_answers(q_answers, valid_answer_set):
+    pass
+
+def vqa_processing(image_dir, annotation_file, question_file, valid_answer_set, image_set):
+    pass
