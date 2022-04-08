971a16cc4280d68b33323240a8f36da79a13fe4e
Lawali <ingu9981@naver.com>
2022-04-09 00:22:00 +0900

    loader.py 모듈 추가

--- VQA_Ingu/loader.py
+++ VQA_Ingu/loader.py
@@ -6,4 +6,14 @@ import torchvision.transforms as transforms
 from PIL import Image
 
 class VQADataset(data.Dataset):
-    
+    def __init__(self, input_dir, input_vqa, max_qst_length=30, max_num_ans=10, transform=None):
+        pass
+
+    def __getitem__(self, idx):
+        pass
+
+    def __len__(self):
+        pass
+
+def get_loader(input_dir, input_vqa_train, input_vqa_valid, max_qst_length, max_num_ans, batch_size, num_workers):
+    pass
