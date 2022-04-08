5c06d3d7eaeeb5dd2008ebde6ba8b608f41a5b20
Lawali <ingu9981@naver.com>
2022-04-09 00:25:50 +0900

    preprocess.py 추상화

---
+++ VQA_Ingu/preprocess.py
@@ -0,0 +1,17 @@
+import os
+import argparse
+import numpy as np
+import torch
+from util import text_helper
+
+def main(args):
+    pass
+
+if __name__=='main':
+    parser = argparse.ArgumentParser()
+
+    # TODO : argument 추가
+
+    args = parser.parse_args()
+
+    main(args)
