e30eb896041036011f6ddecc033fbe3b3c89ad0a
Lawali <ingu9981@naver.com>
2022-04-09 01:13:37 +0900

    image_preprocess.py 작성

--- VQA_Ingu/loader.py
+++ VQA_Ingu/loader.py
@@ -5,6 +5,7 @@ import torch.utils.data as data
 import torchvision.transforms as transforms
 from PIL import Image
 
+
 class VQADataset(data.Dataset):
     def __init__(self, input_dir, input_vqa, max_qst_length=30, max_num_ans=10, transform=None):
         pass
@@ -14,6 +15,7 @@ class VQADataset(data.Dataset):
 
     def __len__(self):
         pass
+
 
 def get_loader(input_dir, input_vqa_train, input_vqa_valid, max_qst_length, max_num_ans, batch_size, num_workers):
     pass
--- VQA_Ingu/util/image_preprocess.py
+++ VQA_Ingu/util/image_preprocess.py
@@ -1,7 +1,24 @@
 import os
-import argparse
 from PIL import Image
 
 
 def resize_images(input_dir, output_dir, size):
-    pass
+    assert os.path.isdir(input_dir)
+    assert os.path.isdir(output_dir)
+    assert size > 0
+    for dir in os.scandir(input_dir):
+        if not dir.is_dir():
+            continue
+        if not os.path.exists(output_dir + '/' + dir.name):
+            os.makedirs(output_dir + '/' + dir.name)
+        images = os.listdir(dir.path)
+        n_images = len(images)
+        for idx, image in enumerate(images):
+            with open(os.path.join(dir.path, image), 'r+b') as f:
+                with Image.open(f) as img:
+                    img = img.resize(size, Image.ANTIALIAS)
+                    img.save(os.path.join(output_dir + '/' + dir.name, image), img.format)
+            if (idx + 1) % 1000 == 0:
+                print("[{}/{}] Resized the images and saved into '{}'."
+                      .format(idx + 1, n_images, output_dir + '/' + dir.name))
+
