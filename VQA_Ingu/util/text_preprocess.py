b9cd2aa3bf399e61203b258ece20c02f1d26ea6f
Lawali <ingu9981@naver.com>
2022-04-09 17:59:21 +0900

    make_vocab_answers.py 작성

--- VQA_Ingu/util/text_preprocess.py
+++ VQA_Ingu/util/text_preprocess.py
@@ -1,7 +1,4 @@
-import math
 import os
-import argparse
-import numpy as np
 import json
 import re
 
@@ -24,8 +21,27 @@ def make_vocab_questions(input_dir, output_dir):
     vocab_list.insert(1, '<unk>')
 
     with open(output_dir + 'vocab_questions.txt', 'w') as f:
-        f.writelines([w+'\n' for w in vocab_list])
+        f.writelines([w + '\n' for w in vocab_list])
 
 
 def make_vocab_answers(input_dir, n_answers, output_dir):
-    pass
+    answers = {}
+    datasets = os.listdir(input_dir)
+    for dataset in datasets:
+        with open(input_dir + '/' + dataset) as f:
+            annotations = json.load(f)['annotations']
+        for annotation in annotations:
+            for answer in annotation['answers']:
+                word = answer['answer']
+                if re.search(r"[^\w\s]", word):
+                    continue
+                if word not in answers:
+                    answers[word] = 0
+                answers[word] += 1
+
+    answers = sorted(answers.items(), key=lambda item: item[1], reverse=True)
+    assert('<unk>' not in answers)
+    top_answers = ['<unk>'] + answers[:n_answers - 1]
+
+    with open(output_dir + 'vocab_answers.txt', 'w') as f:
+        f.writelines([w + '\n' for w in top_answers])
