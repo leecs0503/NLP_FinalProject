56e745d327bd619375730692c9ebc3055ed5610f
Lawali <ingu9981@naver.com>
2022-04-10 01:08:59 +0900

    loader.py 작성

--- VQA_Ingu/loader.py
+++ VQA_Ingu/loader.py
@@ -10,7 +10,7 @@ from util import text_helper
 class VQADataset(data.Dataset):
     def __init__(self, input_dir, input_vqa, max_qst_length=30, max_num_ans=10, transform=None):
         self.input_dir = input_dir
-        self.vqa = np.load(os.path.join(input_dir, input_vqa))
+        self.vqa = np.load(os.path.join(input_dir, input_vqa), allow_pickle=True)
         self.question_vocabulary = text_helper.VocabDict(os.path.join(input_dir, 'vocab_questions.txt'))
         self.answer_vocabulary = text_helper.VocabDict(os.path.join(input_dir, 'vocab_answers.txt'))
         self.max_question_length = max_qst_length
@@ -30,7 +30,7 @@ class VQADataset(data.Dataset):
             sample['answer_label'] = np.random.choice(ans2idc)
             mul2idc = [-1] * self.max_num_ans
             mul2idc[:len(ans2idc)] = ans2idc
-            sample['answer_multiple_choice'] = mul2idc
+            sample['answer_multi_choice'] = mul2idc
 
         if self.transform:
             sample['image'] = self.transform(sample['image'])
@@ -42,4 +42,33 @@ class VQADataset(data.Dataset):
 
 
 def get_loader(input_dir, input_vqa_train, input_vqa_valid, max_qst_length, max_num_ans, batch_size, num_workers):
-    pass
+    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
+
+    vqa_dataset = {
+        'train' : VQADataset(
+            input_dir=input_dir,
+            input_vqa=input_vqa_train,
+            max_qst_length=max_qst_length,
+            max_num_ans=max_num_ans,
+            transform=transform
+        ),
+        'valid' : VQADataset(
+            input_dir=input_dir,
+            input_vqa=input_vqa_valid,
+            max_qst_length=max_qst_length,
+            max_num_ans=max_num_ans,
+            transform=transform
+        )
+    }
+
+    data_loader = {
+        phase : torch.utils.data.DataLoader(
+            dataset=vqa_dataset[phase],
+            batch_size=batch_size,
+            shuffle=True,
+            num_workers=num_workers
+        )
+        for phase in ['train', 'valid']
+    }
+
+    return data_loader
--- VQA_Ingu/models.py
+++ VQA_Ingu/models.py
@@ -3,9 +3,9 @@ import torch.nn as nn
 import torchvision.models as models
 
 
-class ImageModel(nn.modules):
+class ImageModel(nn.Module):
     def __init__(self, embed_size):
-        super(self, ImageModel).__init__()
+        super(ImageModel, self).__init__()
         model = models.resnet101(pretrained=True)
         in_features = model.fc.in_features
         self.model = nn.Sequential(*(list(model.children())[:-1]))
@@ -15,15 +15,16 @@ class ImageModel(nn.modules):
     def forward(self, image):
         with torch.no_grad():
             image_feature = self.model(image)
-        norm = torch.linalg.norm(A=image_feature, dim=1, keepdim=True).detach()
-        image_feature = image_feature.div(norm)
+        image_feature = image_feature.squeeze()
         image_feature = self.fc(image_feature)
+        norm = torch.linalg.vector_norm(image_feature, dim=1, keepdim=True).detach()
+        image_feature = image_feature.div(norm)
         return image_feature
 
 
-class QuestionModel(nn.modules):
+class QuestionModel(nn.Module):
     def __init__(self, question_vocabulary_size, word_embed_size, embed_size, num_layer, hidden_layer_size):
-        super(self, QuestionModel).__init__()
+        super(QuestionModel, self).__init__()
         self.input_embedding = nn.Embedding(question_vocabulary_size, word_embed_size)
         self.tanh = nn.Tanh()
         self.lstm = nn.LSTM(word_embed_size, hidden_layer_size, num_layer)
@@ -32,30 +33,33 @@ class QuestionModel(nn.modules):
     def forward(self, question):
         question_vector = self.input_embedding(question)
         question_vector = self.tanh(question_vector)
+        question_vector = question_vector.transpose(0, 1)
         _, (hidden, cell) = self.lstm(question_vector)
-        output_feature = torch.cat((hidden, cell))
+        output_feature = torch.cat((hidden, cell), 2)
+        output_feature = output_feature.transpose(0, 1)
+        output_feature = output_feature.reshape(output_feature.size()[0], -1)
         output_feature = self.tanh(output_feature)
         output_feature = self.fc(output_feature)
         return output_feature
 
 
-class VQAModel(nn.modules):
+class VQAModel(nn.Module):
     def __init__(
         self,
         embed_size,
         question_vocabulary_size,
         answer_vocabulary_size,
         word_embed_size,
-        num_layer,
+        num_layers,
         hidden_layer_size
     ):
-        super(self, VQAModel).__init__()
+        super(VQAModel, self).__init__()
         self.image_encoder = ImageModel(embed_size)
         self.question_encoder = QuestionModel(
             question_vocabulary_size,
             word_embed_size,
             embed_size,
-            num_layer,
+            num_layers,
             hidden_layer_size
         )
         self.softmax = nn.Softmax()
