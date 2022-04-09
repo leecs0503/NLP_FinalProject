94325b653e1b35b858fddf4fa7ace66a43e1dfe0
Lawali <ingu9981@naver.com>
2022-04-09 17:42:19 +0900

    make_vocab_questions 작성

--- VQA_Ingu/util/text_preprocess.py
+++ VQA_Ingu/util/text_preprocess.py
@@ -1,3 +1,4 @@
+import math
 import os
 import argparse
 import numpy as np
@@ -5,9 +6,26 @@ import json
 import re
 
 
-def make_vocab_questions(input_dir):
-    pass
+def make_vocab_questions(input_dir, output_dir):
+    SENTENCE_SPLIT_REGEX = re.compile(r'(\W+)')
+    vocab_set = set()
+    datasets = os.listdir(input_dir)
+    max_length = 0
+    for dataset in datasets:
+        with open(input_dir + '/' + dataset) as f:
+            questions = json.load(f)['questions']
+        for question in questions:
+            words = SENTENCE_SPLIT_REGEX.split(question['question'].lower())
+            words = [w.strip() for w in words if len(w.strip()) > 0]
+            vocab_set.update(words)
+            max_length = max(max_length, len(words))
+    vocab_list = list(vocab_set).sort()
+    vocab_list.insert(0, '<pad>')
+    vocab_list.insert(1, '<unk>')
+
+    with open(output_dir + 'vocab_questions.txt', 'w') as f:
+        f.writelines([w+'\n' for w in vocab_list])
 
 
-def make_vocab_answers(input_dir, n_answers):
+def make_vocab_answers(input_dir, n_answers, output_dir):
     pass
