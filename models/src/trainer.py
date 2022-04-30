d9503ebe1b61d9becae39826a8cec502f5fda82b
lee changsu <leecs0503@naver.com>
2022-04-30 16:48:04 +0900

    feat(trainer): tensorboard 연동

--- models/src/trainer.py
+++ models/src/trainer.py
@@ -4,8 +4,9 @@ import torch
 import os
 import torch.nn as nn
 import torch.optim as optim
+from typing import Literal
 from torch.optim import lr_scheduler
-
+from torch.utils.tensorboard import SummaryWriter
 
 class VQA_Trainer():
     """
@@ -16,12 +17,14 @@ class VQA_Trainer():
         model: Model,
         data_loader: VQA_DataLoader,
         model_path: str,
+        tensorboard_path: str = 'runs/',
     ):
         # todo: device를 context로
         self.device = device
         self.model = model
         self.data_loader = data_loader
         self.model_path = model_path
+        self.writer = SummaryWriter(tensorboard_path)
 
     def load_model(
         self,
@@ -37,19 +40,52 @@ class VQA_Trainer():
         save_path = os.path.join(self.model_path, f"{self.model.get_name()}-epoch-{epoch:02}.ckpt")
         torch.save({"epoch": epoch + 1, "state_dict": self.model.state_dict()}, save_path)
 
+    def log_batch(
+        self,
+        loss: float,
+        phase: Literal['train', 'valid'],
+        num_epochs,
+        epoch: int,
+        batch_idx: int,
+    ):
+        self.writer.add_scalar(f"Step/Loss/{phase.upper()}-{epoch:02}", loss, batch_idx)
+        print(
+            "| {} SET | Epoch [{:02d}/{:02d}], Step [{:04d}/{:04d}], Loss: {:.4f}".format(
+                phase.upper(),
+                epoch + 1,
+                num_epochs,
+                batch_idx,
+                self.data_loader[phase].batch_step_size,
+                loss.item(),
+            )
+        )
+    
+    def log_step(
+        self,
+        epoch_loss: float,
+        epoch_acc_exp: float,
+        phase: Literal['train', 'valid'],
+        epoch: int,
+        num_epochs: int,
+    ):
+        self.writer.add_scalar(f"Epoch/Loss/{phase.upper()}", epoch_loss, epoch)
+        self.writer.add_scalar(f"Epoch/ACC/{phase.upper()}", epoch_acc_exp, epoch)
+        print(
+            f"| {phase.upper()} SET | Epoch [{epoch + 1:02}/{num_epochs:02}], Loss: {epoch_loss:.4}, Acc(Exp): {epoch_acc_exp:.4}"
+        )
+        pass
+
     def step(
         self,
         epoch: int,
         num_epochs: int,
-        batch_size: int,
         criterion: nn.CrossEntropyLoss,
         optimizer: optim.Adam,
         scheduler: lr_scheduler.StepLR,
     ):
         for phase in self.data_loader:  # equal to: for phase in ['train', 'valid']:
             running_loss = 0.0
-            running_corr_exp1 = 0
-            running_corr_exp2 = 0
+            running_corr_exp= 0
 
             if phase == "train":
                 scheduler.step()
@@ -68,55 +104,40 @@ class VQA_Trainer():
                 optimizer.zero_grad()
 
                 with torch.set_grad_enabled(phase == "train"):
-
                     output = self.model(image, question)  # [batch_size, ans_vocab_size=1000]
-                    _, pred_exp1 = torch.max(output, 1)  # [batch_size]
-                    _, pred_exp2 = torch.max(output, 1)  # [batch_size]
+                    _, pred_exp = torch.max(output, 1)  # [batch_size]
                     loss = criterion(output, label)
 
                     if phase == "train":
                         loss.backward()
                         optimizer.step()
-
-                # Evaluation metric of 'multiple choice'
-                # Exp1: our model prediction to '<unk>' IS accepted as the answer.
-                # Exp2: our model prediction to '<unk>' is NOT accepted as the answer.
-                ans_unk_idx = -1 # todo: tempcode
-                pred_exp2[pred_exp2 == ans_unk_idx] = -9999
+                
+                # pred_exp[pred_exp == ans_unk_idx] = -9999
                 running_loss += loss.item()
-                running_corr_exp1 += (
-                    torch.stack([(ans == pred_exp1.cpu()) for ans in multi_choice])
+                running_corr_exp += (
+                    torch.stack([(ans == pred_exp.cpu()) for ans in multi_choice])
                     .any(dim=0)
                     .sum()
                 )
-                running_corr_exp2 += (
-                    torch.stack([(ans == pred_exp2.cpu()) for ans in multi_choice])
-                    .any(dim=0)
-                    .sum()
+                self.log_batch(
+                        loss=loss,
+                        phase=phase,
+                        num_epochs=num_epochs,
+                        epoch=epoch,
+                        batch_idx=batch_idx
                 )
-
-                # Print the average loss in a mini-batch.
-                if batch_idx % 1 == 0:
-                    print(
-                        "| {} SET | Epoch [{:02d}/{:02d}], Step [{:04d}/{:04d}], Loss: {:.4f}".format(
-                            phase.upper(),
-                            epoch + 1,
-                            num_epochs,
-                            batch_idx,
-                            self.data_loader[phase].batch_step_size,
-                            loss.item(),
-                        )
-                    )
-            """
             # Print the average loss and accuracy in an epoch.
             epoch_loss = running_loss / self.data_loader[phase].batch_step_size
-            epoch_acc_exp1 = running_corr_exp1.double() / len(
+            epoch_acc_exp = running_corr_exp.double() / len(
                 self.data_loader[phase].dataset
-            )  # multiple choice
-            epoch_acc_exp2 = running_corr_exp2.double() / len(
-                self.data_loader[phase].dataset
-            )  # multiple choice
-            """
+            )
+            self.log_step(
+                epoch_loss=epoch_loss,
+                epoch_acc_exp=epoch_acc_exp,
+                phase=phase,
+                epoch=epoch,
+                num_epochs=num_epochs,
+            )
 
 
     def run(
