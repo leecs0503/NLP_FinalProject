51cff43bf995110f6eed39d53d5dacbe7f30c3c0
lee changsu <leecs0503@naver.com>
2022-05-07 01:23:33 +0900

    feat(vgg19_lstm): vgg19 + lstm 기반 vqa 모델 기본 코드 틀 구현

---
+++ VQA/src/model/VGG19_LSTM.py
@@ -0,0 +1,28 @@
+from torch import nn
+
+
+class ImageChannel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def forward(self):
+        pass
+
+
+class TextChannel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def forward(self):
+        pass
+
+
+class LSTM_VQA(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def forward(self):
+        pass
