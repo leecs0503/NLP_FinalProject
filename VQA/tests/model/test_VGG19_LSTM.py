2dab22fb0449c7f1e1c0c27fa5e8b8d668779616
lee changsu <leecs0503@naver.com>
2022-05-07 22:23:14 +0900

    test(vgg_lstm): lstm_vqa 클래스에 대한 테스트코드 작성

--- VQA/tests/model/test_VGG19_LSTM.py
+++ VQA/tests/model/test_VGG19_LSTM.py
@@ -1,12 +1,11 @@
-from src.model.VGG19_LSTM import ImageChannel, TextChannel
+from src.model.VGG19_LSTM import ImageChannel, TextChannel, LSTM_VQA
 import torch
 
 
 def test_ImageChannel():
     batch_size, embed_size = 3, 10
 
-    model = ImageChannel(embed_size)
-    model.to("cpu")
+    model = ImageChannel(embed_size).to("cpu")
     model.eval()
 
     image = torch.zeros((batch_size, 3, 244, 244))
@@ -18,13 +17,12 @@ def test_TextChannel():
     embed_size = 10
 
     model = TextChannel(
-        ques_vocab_size=10,
+        qst_vocab_size=10,
         word_embed_size=20,
         hidden_size=8,
         num_layers=2,
         embed_size=embed_size,
-    )
-    model.to("cpu")
+    ).to("cpu")
     model.eval()
 
     # batch_size = 2
@@ -40,4 +38,23 @@ def test_TextChannel():
 
 
 def test_LSTM_VQA():
-    pass
+    batch_size, embed_size, ans_vocab_size = 2, 10, 5
+    model = LSTM_VQA(
+        embed_size=embed_size,
+        qst_vocab_size=10,
+        word_embed_size=20,
+        num_layers=2,
+        hidden_size=8,
+        ans_vocab_size=ans_vocab_size,
+        dropout_rate=0.5,
+    ).to("cpu")
+    model.eval()
+    image = torch.zeros((batch_size, 3, 244, 244))
+    question = torch.stack(
+        [
+            torch.tensor([3, 1, 2, 0, 0, 0], dtype=torch.int32, device="cpu"),
+            torch.tensor([5, 2, 3, 4, 1, 0], dtype=torch.int32, device="cpu"),
+        ]
+    )
+    res = model(question=question, image=image)
+    assert res.shape == torch.Size([batch_size, ans_vocab_size])
