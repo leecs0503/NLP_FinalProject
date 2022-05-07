fff9e268284127b70b4caa92c628a1b9f916e1ea
lee changsu <leecs0503@naver.com>
2022-05-07 19:32:02 +0900

    test(vgg19_lstm): imageChannel과 TextChannel에 대한 테스트 코드 추가

--- VQA/tests/model/test_VGG19_LSTM.py
+++ VQA/tests/model/test_VGG19_LSTM.py
@@ -1,15 +1,42 @@
-# from src.model.VGG19_LSTM import ImageChannel, TextChannel, LSTM_VQA
+from src.model.VGG19_LSTM import ImageChannel, TextChannel
 import torch
 
 
 def test_ImageChannel():
-    # model = ImageChannel(10)
-    image = torch.zeros((3, 1, 2))
-    print(image)
+    batch_size, embed_size = 3, 10
+
+    model = ImageChannel(embed_size)
+    model.to("cpu")
+    model.eval()
+
+    image = torch.zeros((batch_size, 3, 244, 244))
+    res = model(image)
+    assert res.shape == torch.Size([batch_size, embed_size])
 
 
 def test_TextChannel():
-    pass
+    embed_size = 10
+
+    model = TextChannel(
+        ques_vocab_size=10,
+        word_embed_size=20,
+        hidden_size=8,
+        num_layers=2,
+        embed_size=embed_size,
+    )
+    model.to("cpu")
+    model.eval()
+
+    # batch_size = 2
+    question = torch.stack(
+        [
+            torch.tensor([3, 1, 2, 0, 0, 0], dtype=torch.int32, device="cpu"),
+            torch.tensor([5, 2, 3, 4, 1, 0], dtype=torch.int32, device="cpu"),
+        ]
+    )
+
+    res = model(question)
+    assert res.shape == torch.Size([2, embed_size])
 
 
 def test_LSTM_VQA():
