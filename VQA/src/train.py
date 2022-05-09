bcf64e1cc703b2d7533c5b77e50be67fa16cfe50
Lawali <ingu9981@naver.com>
2022-05-09 17:24:55 +0900

    모델 학습 틀 작성

---
+++ VQA/src/train.py
@@ -0,0 +1,26 @@
+import argparse
+
+# from .trainer import VQA_trainer
+
+
+def train_model():
+    """모델을 학습시키는 메소드
+
+    Args:
+        TODO
+
+    """
+    pass
+
+
+def get_argument() -> argparse.Namespace:
+    pass
+
+
+def main():
+    # args = get_argument()
+    train_model()
+
+
+if __name__ == "__main__":
+    main()
